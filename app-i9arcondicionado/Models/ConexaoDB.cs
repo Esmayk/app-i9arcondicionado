572a8588bc5a58f63a18653ec71795f5f70a7f46
Esmayk <esmaykalves@gmail.com>
2018-03-05 11:59:57 -0300

    Altera Conexao

--- app-i9arcondicionado/Models/ConexaoDB.cs
+++ app-i9arcondicionado/Models/ConexaoDB.cs
@@ -7,5 +7,12 @@ namespace app_i9arcondicionado.Models
 {
     public class ConexaoDB
     {
+        public NpgsqlConnection ConexaoPostgreSQL(int sistema)
+        {
+            conexao = "Server = localhost; Port = 5432; User Id = postgres; Password = postgres; Database = i9arcondicionado;";
+            NpgsqlConnection conn = new NpgsqlConnection(String.Format(conexao));
+            conn.Open();
+            return conn;
+        }
     }
 }
