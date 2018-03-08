0a104788d00ec0aa36d45ab8cf71688863f77e1e
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-07 22:23:09 -0300

    Altera Estado civil

--- app-i9arcondicionado/Controllers/EstadoCivilController.cs
+++ app-i9arcondicionado/Controllers/EstadoCivilController.cs
@@ -10,36 +10,34 @@ using System.Web.Http;
 namespace app_i9arcondicionado.Controllers
 {
     [RoutePrefix("api/i9arcondicionado")]
-    public class TipoTelefoneController : ApiController
+    public class EstadoCivilController : ApiController
     {
-
-        private TipoTelefone tipoTelefone;
-        private List<TipoTelefone> tipoTelefoneList = new List<TipoTelefone>();
+        private EstadoCivil estadoCivil;
+        private List<EstadoCivil> estadoCivilList = new List<EstadoCivil>();
         private NpgsqlDataReader leitor;
         private NpgsqlCommand query;
 
         [HttpGet]
-        [Route("tipoTelefone")]
-        public IHttpActionResult getTipoTelefone()
+        [Route("estado")]
+        public IHttpActionResult getEstadoCivil()
         {
-
             NpgsqlConnection conexao = new ConexaoDB().ConexaoPostgreSQL();
             if (conexao != null)
             {
-                string consulta = "select * from tipo_telefone";
+                string consulta = "select * from estado_civil";
                 query = new NpgsqlCommand(consulta, conexao);
                 try
                 {
                     leitor = query.ExecuteReader();
                     while (leitor.Read())
                     {
-                        tipoTelefone = new TipoTelefone
+                        estadoCivil = new EstadoCivil
                         {
                             Id = leitor.GetDecimal(0),
-                            Descricao = leitor.GetString(1)
+                            Descricao = leitor.GetString(1),
                             
                         };
-                        tipoTelefoneList.Add(tipoTelefone);
+                        estadoCivilList.Add(estadoCivil);
                     }
                 }
                 catch (Exception ex)
@@ -55,7 +53,7 @@ namespace app_i9arcondicionado.Controllers
             {
                 throw new Exception("Conexão é nula");
             }
-            return Ok(tipoTelefoneList);
+            return Ok(estadoCivilList);
         }
     }
 }
