2b0e0a10c80317ed04203e54eb7a90e9e523ca9b
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-07 22:36:10 -0300

    Cria Tipo Pessoa Controller

---
+++ app-i9arcondicionado/Controllers/TipoPessoaController.cs
@@ -0,0 +1,57 @@
+﻿using app_i9arcondicionado.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace app_i9arcondicionado.Controllers
+{
+    [RoutePrefix("api/i9arcondicionado")]
+    public class TipoPessoaController : ApiController
+    {
+        private TipoPessoa tipoPessoa;
+        private List<TipoPessoa> tipoPessoaList = new List<TipoPessoa>();
+        private NpgsqlDataReader leitor;
+        private NpgsqlCommand query;
+
+        [HttpGet]
+        [Route("tipoPessoa")]
+        public IHttpActionResult getTipoPessoa()
+        {
+
+            NpgsqlConnection conexao = new ConexaoDB().ConexaoPostgreSQL();
+            if (conexao != null)
+            {
+                string consulta = "select * from tipo_pessoa";
+                query = new NpgsqlCommand(consulta, conexao);
+                try
+                {
+                    leitor = query.ExecuteReader();
+                    while (leitor.Read())
+                    {
+                        tipoPessoa = new TipoPessoa
+                        {
+                            Id = leitor.GetDecimal(0),
+                            Descricao = leitor.GetString(1)
+
+                        };
+                        tipoPessoaList.Add(tipoPessoa);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+            }
+            else
+            {
+                throw new Exception("Conexão é nula");
+            }
+            return Ok(tipoPessoa);
+        }
+    }
+}
