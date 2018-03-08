68ed541c96ede3808c0930a2237437c0723ee31b
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-07 22:02:56 -0300

    Altera estado civil

--- app-i9arcondicionado/Models/EstadoCivil.cs
+++ app-i9arcondicionado/Models/EstadoCivil.cs
@@ -7,5 +7,28 @@ namespace app_i9arcondicionado.Models
 {
     public class EstadoCivil
     {
+        private Decimal id;
+        private String descricao;
+
+        public EstadoCivil()
+        {
+
+        }
+        public EstadoCivil(Decimal id, String descricao)
+        {
+            this.Id = id;
+            this.Descricao = descricao;
+        }
+        public Decimal Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
     }
 }
