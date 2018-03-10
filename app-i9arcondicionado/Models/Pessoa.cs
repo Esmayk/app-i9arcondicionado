cc284d9e4a9d7d1431521a7c45c3dd626166a91f
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-09 23:20:17 -0300

    Cria Pessoa

---
+++ app-i9arcondicionado/Models/Pessoa.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app_i9arcondicionado.Models
+{
+    public class Pessoa
+    {
+        private Decimal id;
+        private String nome;
+        private String cpf;
+        private String sexo;
+        private DateTime nascimento;
+        private String mae;
+        private String pai;
+        private DateTime dataCadastro;
+        private String status;
+        private Decimal estadoCivilFk;
+        private List<Telefone> telefoneList;
+        private List<Endereco> enderecoList;
+
+        public Pessoa()
+        {
+
+        }
+        public Pessoa(Decimal id, String nome, String cpf, String sexo, DateTime nascimento, String mae, String pai, DateTime dataCadastro, String status, Decimal estadoCivilFk, List<Telefone> telefoneList, List<Endereco> enderecoList)
+        {
+            this.Id = id;
+            this.Cpf = cpf;
+            this.Sexo = sexo;
+            this.Nascimento = nascimento;
+            this.Mae = mae;
+            this.Pai = pai;
+            this.DataCadastro = dataCadastro;
+            this.Status = status;
+            this.EstadoCivilFk = estadoCivilFk;
+        }
+        public Decimal Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+
+        public String Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+
+
+        public String Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+
+
+        public DateTime Nascimento
+        {
+            get { return nascimento; }
+            set { nascimento = value; }
+        }
+
+
+        public String Mae
+        {
+            get { return mae; }
+            set { mae = value; }
+        }
+
+
+        public String Pai
+        {
+            get { return pai; }
+            set { pai = value; }
+        }
+
+
+        public DateTime DataCadastro
+        {
+            get { return dataCadastro; }
+            set { dataCadastro = value; }
+        }
+
+
+        public String Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        public Decimal EstadoCivilFk
+        {
+            get { return estadoCivilFk; }
+            set { estadoCivilFk = value; }
+        }
+
+        public List<Telefone> TelefoneList
+        {
+            get { return telefoneList; }
+            set { telefoneList = value; }
+        }
+
+        public List<Endereco> EnderecoList
+        {
+            get { return enderecoList; }
+            set { enderecoList = value; }
+        }
+    }
+}
