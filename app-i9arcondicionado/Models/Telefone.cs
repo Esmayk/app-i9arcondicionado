fb876b7674b97009ae0e3179ac71929b4106cff2
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-09 22:41:28 -0300

    Cria telefone

---
+++ app-i9arcondicionado/Models/Telefone.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app_i9arcondicionado.Models
+{
+    public class Telefone
+    {
+        private Decimal id;
+        private String numero;
+        private Decimal pessoaFk;
+        private Decimal tipoTelefoneFk;
+
+        public Telefone()
+        {
+
+        }
+
+        public Telefone(Decimal id, String numero, Decimal pessoaFk, Decimal tipoTelefoneFk)
+        {
+            this.id = id;
+            this.numero = numero;
+            this.pessoaFk = pessoaFk;
+            this.tipoTelefoneFk = tipoTelefoneFk;
+        }
+
+        public Decimal Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public Decimal PessoaFk
+        {
+            get { return pessoaFk; }
+            set { pessoaFk = value; }
+        }
+        public Decimal TipoTelefoneFk
+        {
+            get { return tipoTelefoneFk; }
+            set { tipoTelefoneFk = value; }
+        }
+    }
+}
