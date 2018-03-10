984034e2b839bdfaab59c2fb90a1a09ed311dfe3
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-09 23:28:26 -0300

    Cria Post Pessoa

--- app-i9arcondicionado/Controllers/PessoaController.cs
+++ app-i9arcondicionado/Controllers/PessoaController.cs
@@ -10,5 +10,11 @@ namespace app_i9arcondicionado.Controllers
     [RoutePrefix("api/i9arcondicionado")]
     public class PessoaController : ApiController
     {
+        [HttpPost]
+        [Route("pessoa")]
+        public IHttpActionResult postPessoa()
+        {
+            return Ok("Pessoa Cadastrada Com Sucesso.");
+        }
     }
 }
