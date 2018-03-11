0ab4b9eaa1e4e0befef21f159095efa559096a78
esmayk.tillesse <esmaykalves@gmail.com>
2018-03-11 20:39:35 -0300

    Cria pessoa tipo Model.

---
+++ app-i9arcondicionado/Models/PessoaTipo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace app_i9arcondicionado.Models
+{
+    public class PessoaTipo
+    {
+        private Decimal id;
+        private Decimal pessoaFk;
+        private Decimal tipoPessoaFk;
+        
+        public PessoaTipo()
+        {
+
+        }
+
+        public PessoaTipo(Decimal id, Decimal pessoaFk, Decimal tipoPessoaFk)
+        {
+            this.Id = id;
+            this.PessoaFk = pessoaFk;
+            this.TipoPessoaFk = tipoPessoaFk;
+        }
+        public Decimal Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        public Decimal PessoaFk
+        {
+            get { return pessoaFk; }
+            set { pessoaFk = value; }
+        }
+
+        public Decimal TipoPessoaFk
+        {
+            get { return tipoPessoaFk; }
+            set { tipoPessoaFk = value; }
+        }
+
+    }
+}
