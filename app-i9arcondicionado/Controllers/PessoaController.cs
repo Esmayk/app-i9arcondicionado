6125198351374782a1cf92f166b04a4f622d3b95
Esmayk <esmaykalves@gmail.com>
2018-03-12 16:04:46 -0300

    Cria Serviço de get Pessoa por id.

--- app-i9arcondicionado/Controllers/PessoaController.cs
+++ app-i9arcondicionado/Controllers/PessoaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Web.Http;
 using app_i9arcondicionado.Models;
+using Newtonsoft.Json;
 using Npgsql;
 
 namespace app_i9arcondicionado.Controllers
@@ -15,6 +16,75 @@ namespace app_i9arcondicionado.Controllers
         private NpgsqlDataReader leitor;
         private NpgsqlCommand query;
 
+        [HttpGet]
+        [Route("pessoa/{id}")]
+        public IHttpActionResult getPessoPorId(Decimal id)
+        {
+            String json = null;
+            NpgsqlConnection conexao = new ConexaoDB().ConexaoPostgreSQL();
+            if (conexao != null)
+            {
+                try { 
+                String consulta = "select row_to_json(p) " +
+                                   "from( " +
+                                   "  select pessoa_pk as id, nome, cpf, sexo, nascimento, mae, pai, data_cadastro as dataCadastro, status, estado_civil_fk as estadoCivilFk, " +
+                                   "    ( " +
+                                   "      select array_to_json(array_agg(row_to_json(te))) " +
+                                   "      from(" +
+                                   "        select telefone_pk as id, numero, pessoa_fk as pessoaFk, " +
+                                   "        tipo_telefone_fk as tipoTelefoneFk  " +
+                                   "        from telefone " +
+                                   "        where telefone.pessoa_fk = pessoa.pessoa_pk  " +
+                                   "      ) te " +
+                                   "    ) as telefoneList, " +
+                                   "      ( " +
+                                   "      select array_to_json(array_agg(row_to_json(en)))  " +
+                                   "      from(" +
+                                   "        select endereco_pk as id, end_descricao as endDescricao, " +
+                                   "        complemento, bairro, cep, cidade_fk as cidadeFk, " +
+                                   "        pessoa_fk as pessoaFk, numero  " +
+                                   "        from endereco  " +
+                                   "        where endereco.pessoa_fk = pessoa.pessoa_pk  " +
+                                   "      ) en  " +
+                                   "    ) as enderecoList, " +
+                                   "    ("+
+                                   "    select row_to_json(pt)"+
+                                   "    from("+
+                                   "      select pessoa_tipo_pk as id, pessoa_fk as pessoaFk, tipo_pessoa_fk as tipoPessoaFk"+
+                                   "      from pessoa_tipo"+
+                                   "      where pessoa_tipo.pessoa_fk = pessoa.pessoa_pk"+
+                                   "        )pt"+
+                                   "    ) as pessoaTipoFk"+
+                                   "  from pessoa  " +
+                                   "  where pessoa_pk =:id  " +
+                                   ") p";
+                    query = new NpgsqlCommand(consulta, conexao);
+                    query.Parameters.Add(new NpgsqlParameter("id", DbType.Decimal));
+                    query.Parameters[0].Value = id;
+                    leitor = query.ExecuteReader();
+                    while (leitor.Read())
+                    {
+                        json = leitor.GetString(0);
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+                finally
+                {
+                    conexao.Close();
+
+                }
+            }
+            else
+            {
+                throw new Exception("Conexão é nula");
+            }
+            var result = JsonConvert.DeserializeObject<Pessoa>(json);
+            return Ok(result);
+        }
         [HttpPost]
         [Route("pessoa")]
         public IHttpActionResult postPessoa(Pessoa pessoa)
